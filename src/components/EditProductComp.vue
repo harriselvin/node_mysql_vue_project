342b53c9c55a49185bc2696538ea50691d2d8f9a
asheekasamuels <samuelsasheeka133@gmail.com>
2024-08-21 15:31:41 +0200

    back

--- src/components/EditProductComp.vue
+++ src/components/EditProductComp.vue
@@ -1,146 +1,110 @@
 <template>
-  <!-- Button trigger modal -->
-  <button
-    type="button"
-    class=""
-    data-bs-toggle="modal"
-    data-bs-target="#editProduct"
-  >
-    Edit Product
-  </button>
-
-  <!-- Modal -->
-  <div
-    class="modal fade"
-    id="editProduct"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">
-            Update Product
-          </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          >
-            X
-          </button>
-        </div>
-        <div class="modal-body">
-
-          <div class="inputs">
-            <label>Product Name</label>
-            <input type="text" v-model="productName" />
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class=""
+      data-bs-toggle="modal"
+      data-bs-target="#editProduct"
+    >
+      Edit Product
+    </button>
+  
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="editProduct"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">
+              Update Product
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            >
+              X
+            </button>
           </div>
-
-          <div class="inputs">
-            <label>Quantity</label>
-            <input type="number" v-model="Quantity" />
+          <div class="modal-body">
+  
+            <div class="inputs">
+              <label>Product Name</label>
+              <input type="text" v-model="productName" />
+            </div>
+  
+            <div class="inputs">
+              <label>Quantity</label>
+              <input type="number" v-model="Quantity" />
+            </div>
+  
+            <div class="inputs">
+              <label>Price</label>
+              <input type="number" v-model="Price" />
+            </div>
+  
+            <div class="inputs">
+              <label>Category</label>
+              <input type="text" v-model="Category" />
+            </div>
+  
+            <div class="inputs">
+              <label>Image URL</label>
+              <input type="url" v-model="imageURL" />
+            </div>
+  
           </div>
-
-          <div class="inputs">
-            <label>Price</label>
-            <input type="number" v-model="Price" />
+          <div class="modal-footer">
+            <button type="button" @click="updateProd(productID)" class="">Save changes</button>
           </div>
-
-          <div class="inputs">
-            <label>Category</label>
-            <input type="text" v-model="Category" />
-          </div>
-
-          <div class="inputs">
-            <label>Image URL</label>
-            <input type="url" v-model="imageURL" />
-          </div>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" @click="updateProd(productID)" class="">Save changes</button>
         </div>
       </div>
     </div>
-  </div>
-</template>
-
-<script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-        productName: "",
-        Quantity: "",
-        Price: "",
-        Category: "",
-        imageURL: "",
-    };
-  },
-  methods: {
-    async updateProd(id) {
-        try {
-            await axios.patch(`https://node-mysql-vue-project-60pu.onrender.com/Products/${id}`, {
-                productName: this.productName,
-                Quantity: this.Quantity,
-                Price: this.Price,
-                Category: this.Category,
-                imageURL: this.imageURL,
-            });
-            this.productName= "";
-            this.Quantity = "";
-            this.Price= ""; 
-            this.Category= "";
-            this.imageURL= "";
-            alert("Product Has Been Updated");
-            window.location.reload();
-        } catch (error) {
-            alert (error);
-        }
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+    data() {
+      return {
+          productName: "",
+          Quantity: "",
+          Price: "",
+          Category: "",
+          imageURL: "",
+      };
     },
-  },
-};
-</script>
+    methods: {
+      async updateProd(id) {
+          try {
+              await axios.patch(` https://node-mysql-vue-project-60pu.onrender.com/Products/${id}`, {
+                  productName: this.productName,
+                  Quantity: this.Quantity,
+                  Price: this.Price,
+                  Category: this.Category,
+                  imageURL: this.imageURL,
+              });
+              this.productName= "";
+              this.Quantity = "";
+              this.Price= ""; 
+              this.Category= "";
+              this.imageURL= "";
+              alert("Product Has Been Updated");
+              window.location.reload();
+          } catch (error) {
+              alert (error);
+          }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
 
-<style scoped>
-    button {
-  background: #ce1212;
-  color: #eeebdd;
-  border: 2px solid #ce1212;
-
-  border-radius: 30px;
-  box-shadow: 0 0 0 0 transparent;
-  -webkit-transition: all 0.2s ease-in;
-  -moz-transition: all 0.2s ease-in;
-  transition: all 0.2s ease-in;
-  margin-top: 15px;
-  margin-bottom: 15px;
-}
-
-button:hover {
-  color: #eeebdd;
-  background: #ce1212;
-  box-shadow: 0 0 30px 5px #ce1212;
-  -webkit-transition: all 0.2s ease-out;
-  -moz-transition: all 0.2s ease-out;
-  transition: all 0.2s ease-out;
-}
-
-.modal-content {
-  background: #1b1717 !important;
-  color: #eeebdd !important;
-  display: block;
-}
-
-input {
-  color: #eeebdd !important;
-  border: 2px solid #ce1212;
-  background: #ce1212 !important;
-  border-radius: 20px;
-  margin: 15px;
-  width: 60%;
-}
-</style>
+  </style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,103 +1,116 @@
 <template>
-  <h1 class="text-center animate__animated animate__fadeInUp">Admin</h1>
-  <div class="userSection">
-    <div class="titles">
-      <h3>Users</h3>
-      <div>
-        <AddUserComp />
+  <div>
+    <h1 class="text-center animate__animated animate__fadeInUp">Admin</h1>
+    
+    <div class="userSection">
+      <div class="titles">
+        <h3>Users</h3>
+        <button @click="showUserModal = true" class="addButton">Add User</button>
+      </div>
+      <div class="userTable">
+        <table>
+          <thead>
+            <tr>
+              <th>userID</th>
+              <th>First Name</th>
+              <th class="remove">Last Name</th>
+              <th class="emailHead">Email Address</th>
+              <th class="remove">Gender</th>
+              <th class="remove">Age</th>
+              <th>Action</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody v-for="user of users" :key="user.userID">
+            <tr v-if="users">
+              <td>{{ user.userID }}</td>
+              <td>{{ user.userName }}</td>
+              <td class="remove">{{ user.lastName }}</td>
+              <td class="emailHead">{{ user.emailAdd }}</td>
+              <td class="remove">{{ user.gender }}</td>
+              <td class="remove">{{ user.age }}</td>
+              <td><button @click="delUser(user.userID)" class="delButton">Delete</button></td>
+              <td><EditUserComp /></td>
+            </tr>
+            <tr v-else>
+              <td colspan="8">No users available</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
-    <div class="userTable">
-      <table>
-        <tr>
-          <th>userID</th>
-          <th>First Name</th>
-          <th class="remove">Last Name</th>
-          <th class="emailHead">Email Address</th>
-          <th class="remove">Gender</th>
-          <th class="remove">Age</th>
-          <th>Action</th>
-          <th>Action</th>
-        </tr>
-        <tbody v-for="user of users" :key="user.userID" :user="user">
-          <tr v-if="users">
-            <td>{{ user.userID }}</td>
-            <td>{{ user.userName }}</td>
-            <td class="remove">{{ user.lastName }}</td>
-            <td class="emailHead">{{ user.emailAdd }}</td>
-            <td class="remove">{{ user.gender }}</td>
-            <td class="remove">{{ user.age }}</td>
-            <td><button @click="delUser(user.userID)" class="delButton">Delete</button></td>
-            <td>
-              <edit-user-comp/>
-            </td>
-          </tr>
-          <tr v-else>
-            no
-          </tr>
-        </tbody>
-      </table>
+  
+    <div class="productSection">
+      <div class="titles">
+        <h3>Products</h3>
+        <button @click="showProductModal = true" class="addButton">Add Product</button>
+      </div>
+      <div class="productsTable">
+        <table>
+          <thead>
+            <tr>
+              <th class="remove">Product ID</th>
+              <th>Product Name</th>
+              <th class="remove">Quantity</th>
+              <th class="remove">Price</th>
+              <th class="imgHead">Category</th>
+              <th class="imgHead">Image</th>
+              <th>Action</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody v-for="product of products" :key="product.productID">
+            <tr v-if="products">
+              <td class="remove">{{ product.productID }}</td>
+              <td>{{ product.productName }}</td>
+              <td class="remove">{{ product.quantity }}</td>
+              <td class="remove">{{ product.price }}</td>
+              <td class="imgHead">{{ product.category }}</td>
+              <td class="imgHead"><img :src="product.imageURL" :alt="product.productName" /></td>
+              <td><button @click="delProduct(product.productID)" class="delButton">Delete</button></td>
+              <td><EditProductComp /></td>
+            </tr>
+            <tr v-else>
+              <td colspan="8">No products available</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
 
-  <div class="productSection">
-    <div class="titles">
-      <h3>Products</h3>
-      <div>
-        <add-product-comp />
-      </div>
-    </div>
-    <div class="productsTable">
-      <table>
-        <thead>
-          <tr>
-            <th class="remove">Product ID</th>
-            <th>Product Name</th>
-            <th class="remove">Quantity</th>
-            <th class="remove">Price</th>
-            <th class="imgHead">Category</th>
-            <th class="imgHead">Image</th>
-            <th>Action</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody
-          v-for="product of products"
-          :key="product.productID"
-          :product="product"
-        >
-          <tr v-if="products">
-            <td class="remove">{{ product.productID }}</td>
-            <td>{{ product.productName }}</td>
-            <td class="remove">{{ product.Quantity }}</td>
-            <td class="remove">{{ product.Price }}</td>
-            <td class="imgHead">{{ product.Category }}</td>
-            <td class="imgHead"><img :src="product.imageURL" :alt="product.productName" /></td>
-            <td>
-              <button @click="delProduct(product.productID)" class="delButton">Delete</button>
-            </td>
-            <td>
-                <edit-product-comp/>
-            </td>
-          </tr>
-          <tr v-else>
-            no
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <!-- User Modal -->
+    <AddUserComp
+      :show="showUserModal"
+      @close="showUserModal = false"
+      @user-added="refreshData"
+    />
+
+    <!-- Product Modal -->
+    <AddProductComp
+      :show="showProductModal"
+      @close="showProductModal = false"
+      @product-added="refreshData"
+    />
   </div>
 </template>
 
 <script>
-import AddUserComp from "@/components/AddUserComp.vue"
-import AddProductComp from "@/components/AddProductComp.vue"
-import EditProductComp from '@/components/EditProductComp.vue'
-import EditUserComp from "@/components/EditUserComp.vue"
-import axios from "axios"
+import AddUserComp from "@/components/AddUserComp.vue";
+import AddProductComp from "@/components/AddProductComp.vue";
+import EditProductComp from '@/components/EditProductComp.vue';
+import EditUserComp from "@/components/EditUserComp.vue";
+import axios from "axios";
+
 export default {
   created() {
-    this.$store.dispatch("getProducts"), this.$store.dispatch("getUsers");
+    this.$store.dispatch("getProducts");
+    this.$store.dispatch("getUsers");
+  },
+  data() {
+    return {
+      showUserModal: false,
+      showProductModal: false,
+    };
   },
   computed: {
     products() {
@@ -110,37 +123,44 @@ export default {
   methods: {
     async delProduct(id) {
       try {
-        await axios.delete(
-          `https://node-mysql-vue-project-60pu.onrender.com/Products/${id}`
-        );
+        await axios.delete(`https://your-api-url/products/${id}`);
         this.$store.dispatch("getProducts");
         alert("Product Removed");
-        window.location.reload();
       } catch (error) {
-        alert(error);
+        alert(error.message);
       }
     },
     async delUser(id) {
       try {
-        await axios.delete(`https://node-mysql-vue-project-60pu.onrender.com/user/${id}`);
+        await axios.delete(`https://your-api-url/users/${id}`);
         this.$store.dispatch("getUsers");
-        alert("This user has been removed");
-        window.location.reload();
+        alert("User Removed");
       } catch (error) {
-        alert(error);
+        alert(error.message);
       }
+    },
+    refreshData() {
+      this.$store.dispatch("getProducts");
+      this.$store.dispatch("getUsers");
     },
   },
   components: {
     AddUserComp,
     AddProductComp,
     EditProductComp,
-    EditUserComp
+    EditUserComp,
   },
 };
 </script>
 
 <style scoped>
+/* General Styles */
+body {
+  font-family: Arial, sans-serif;
+  color: #333; /* Default text color */
+  background-color: #fff; /* Background color */
+}
+
 img {
   height: 80px;
   width: 80px;
@@ -151,15 +171,18 @@ table {
   text-align: center;
   margin: auto;
 }
+
 th {
-  border: 2px solid #ce1212;
+  border: 2px solid #00bcd4; /* Border color */
   width: 150px;
   padding: 10px;
+  background-color: #00bcd4; /* Background color */
+  color: #fff; /* Text color */
 }
 
 td {
   padding: 10px;
-  border: 2px solid #ce1212;
+  border: 2px solid #00bcd4; /* Border color */
 }
 
 .titles {
@@ -168,40 +191,36 @@ td {
 }
 
 .userSection, .productSection {
-    margin: 20px;
+  margin: 20px;
 }
 
 .delButton {
-    background: #ce1212;
-  color: #eeebdd;
-  border: 2px solid #ce1212;
+  background: #00bcd4; /* Background color */
+  color: #fff; /* Text color */
+  border: 2px solid #00bcd4; /* Border color */
   border-radius: 30px;
   box-shadow: 0 0 0 0 transparent;
-  -webkit-transition: all 0.2s ease-in;
-  -moz-transition: all 0.2s ease-in;
   transition: all 0.2s ease-in;
 }
 
 .delButton:hover {
-     color: #eeebdd;
-  background: #ce1212;
-  box-shadow: 0 0 30px 5px #ce1212;
-  -webkit-transition: all 0.2s ease-out;
-  -moz-transition: all 0.2s ease-out;
+  color: #fff; /* Text color */
+  background: #0097a7; /* Slightly darker shade for hover */
+  box-shadow: 0 0 30px 5px #0097a7; /* Slightly darker shade for hover */
   transition: all 0.2s ease-out;
 }
 
+/* Media Queries */
 @media only screen and (max-width: 700px) {
-  .imgHead, .emailHead{
+  .imgHead, .emailHead {
     display: none;
   }
   table {
     width: 100%;
   }
- 
 }
 
-@media only screen and (max-width: 305px){
+@media only screen and (max-width: 305px) {
   .remove {
     display: none;
   }
