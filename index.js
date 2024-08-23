c40f4c3ca512f8a00e56f5d6f1beab08741f8bab
harriselvin <harriselvin6@gmail.com>
2024-08-23 13:23:55 +0200

    installed cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import userRouter from './router/userRouter.js'
 import prodRouter from './router/productRouter.js'
-// import cors from 'cors'
+import cors from 'cors'
 
 const port = process.env.PORT || 8000
 const app = express()
 
-// app.use(cors)
+app.use(cors({
+    origin: 'http://localhost:8000',
+    credentials: true
+}))
 
 app.use(express.static('public'))
 app.use(express.json())
